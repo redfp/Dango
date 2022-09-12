ef862dcfe9dcd251711797883ef9be9ef0f73245
redfp <redfpf1991@gmail.com>
2022-09-12 20:54:11 +0300

    Added test files and updated README

--- README.md
+++ README.md
@@ -1,22 +1,7 @@
 # Dango
-![](https://user-images.githubusercontent.com/50246819/128202000-93d2e294-ceb9-42bf-abe9-6b0168060836.png)
 
-Minimal and simple HTML startpage created for personal use, inspired by [Bento](https://github.com/migueravila/Bento).
+Warning! This branch is still under development, and may be unstable. Please go back to `main` (unless you REALLY want to see the WIP version)
 
+**This branch** has one major goal: to create a GUI way to configure your startpage.
 
-### Goals
-
-- Lightweight but modern
-- Configurable
-- Vanilla HTML / JS / CSS
-- Responsive design
-
-### Usage
-
-These tiles with icons on the main page are called _links_. All _links_ are contained inside _slides_.
-
-Every _link_ can have either an icon, taken from [Feather Icons](https://feathericons.com/), or some text. The text one is useful for things that don't have a suitable icon. You can customise _links_ at `js/links.js`.
-
-Every _slide_ has a title, and can hold as many _links_ as you want. The thing is, if the number of _links_ in a _slide_ exceeds viewable limit (6 links per slide by default), everything will be out of its place. Maybe it will be fixed in future versions. You can change the amount of links one slide can hold in the `css/style.css`, in `:root` section.
-
-In any case, you are free to edit any file you want. Beginner-friendliness is on the to-do list, but not something this startpage has right now. Again, it was created mostly for personal use (and as a training in web development).
+This change will not be backward-compatible with current version.
---
+++ test/config.js
@@ -0,0 +1,84 @@
+var config = {
+  show_seconds: false,
+  am_pm: false,
+  show_year: false,
+  month_first: true,
+  open_links_in_new_tab: true,
+  search_engine: "https://www.google.com/search?q=%s",
+  clear_searchbar: true,
+  force_x_button: true,
+  auto_save: true,
+};
+
+const default_links = [
+  {
+    title: "General",
+    content: [
+      {
+        link: "https://github.com/",
+        icon: true,
+        value: "github",
+      },
+      {
+        link: "https://gitlab.com/",
+        icon: true,
+        value: "gitlab",
+      },
+      {
+        link: "https://trello.com/",
+        icon: true,
+        value: "trello",
+      },
+      {
+        link: "https://drive.google.com/drive/u/0/my-drive",
+        icon: true,
+        value: "hard-drive",
+      },
+      {
+        link: "https://stackoverflow.com/",
+        icon: true,
+        value: "layers",
+      },
+      // {
+      //   link: "https://en.wikipedia.org/wiki/Main_Page",
+      //   icon: false,
+      //   value: "W",
+      // },
+    ],
+  },
+  {
+    title: "Entertainment",
+    content: [
+      {
+        link: "https://www.youtube.com/",
+        icon: true,
+        value: "youtube",
+      },
+      {
+        link: "https://www.reddit.com/",
+        icon: true,
+        value: "home",
+      },
+      {
+        link: "https://music.youtube.com/",
+        icon: true,
+        value: "play-circle",
+      },
+      {
+        link: "https://www.netflix.com/browse",
+        icon: false,
+        value: "N",
+      },
+      {
+        link: "https://www.twitch.tv/",
+        icon: true,
+        value: "twitch",
+      },
+      {
+        link: "https://open.spotify.com",
+        icon: false,
+        value: "S",
+      },
+    ],
+  },
+];
